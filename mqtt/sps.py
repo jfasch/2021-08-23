c230f144d7b25c74f7c0650fe0600288336a61d3
Joerg Faschingbauer <jf@faschingbauer.co.at>
2021-08-25 12:48:16 +0200

    OO: sps/error handler

--- mqtt/sps.py
+++ mqtt/sps.py
@@ -1,17 +1,29 @@
 from paho.mqtt import client
 import json
 import datetime
+import time
+import random
+import uuid
 
+
+class ErrorHandler:
+    def __init__(self, mqtt, topic):
+        self.log = []
+        self.sequence = 0
+        self.mqtt = mqtt
+        self.topic = topic
+    def shit_happened(self, shit):
+        msg = {'shit': shit,
+               'msgid': uuid.uuid4().fields }
+        self.log.append(msg)
+        self.mqtt.publish(self.topic, json.dumps(msg))
 
 c = client.Client()
 c.connect('localhost')
 
-now = datetime.datetime.now()
+errorhandler = ErrorHandler(c, '/spsen/meine-sps')
 
-msg = {
-    'timestamp': now.timestamp(),
-    'text': 'seas oida',
-}
-
-msg_payload = json.dumps(msg)
-c.publish('/spsen/meine-sps', msg_payload)
+while True:
+    bullshit = 'bullshit ' + str(random.randrange(1000))
+    errorhandler.shit_happened(bullshit)
+    time.sleep(1.2)
